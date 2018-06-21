e3a91156d53ad7d66cee632649340283e0e0a7c1
Ari Chivukula <ari@chivuku.la>
2018-06-21 13:08:14 -0700

    Update run.py

--- tests/run.py
+++ tests/run.py
@@ -10,6 +10,6 @@ total_tests = 0
 for line in open("tests/features/TestExamples.feature"):
   if line.startswith("  Scenario:"):
     total_tests += 1
-    pool.apply_async(test_feature, total_tests)
+    pool.apply_async(test_feature, (total_tests, ))
 pool.close()
 pool.join()
