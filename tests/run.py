b3dace50f12e0428b923fb51c96fb2f99622e82d
Ari Chivukula <ari@chivuku.la>
2018-06-21 12:22:09 -0700

    Update run.py

--- tests/run.py
+++ tests/run.py
@@ -1,5 +1,13 @@
+from multiprocessing import Pool
+
+pool = Pool()
 total_tests = 0
 for line in open("tests/features/TestExamples.feature"):
   if line.startswith("  Scenario:"):
     total_tests += 1
-print (total_tests)
+    pool.apply_async(test_feature, total_tests)
+pool.close()
+pool.join()
+
+def test_feature(feature_idx):
+  print (feature_idx)
