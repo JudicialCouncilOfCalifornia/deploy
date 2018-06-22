874eb6505bdf1ef43f591f2a50fd53ff586bd1e0
Ari Chivukula <ari@chivuku.la>
2018-06-21 17:05:59 -0700

    Update run.py

--- tests/run.py
+++ tests/run.py
@@ -2,9 +2,16 @@ from __future__ import print_function
 from multiprocessing import Pool
 import os
 
-def test_feature(feature_idx):
-  print("\tRunning: "+ str(feature_idx + 1))
+def retry_feature(feature_idx):
+  print("\tTrying: "+ str(feature_idx + 1))
   os.system("lettuce --verbosity 1 -s " + str(feature_idx + 1) + " tests/features/TestExamples.feature")
+
+def test_feature(feature_idx):
+  try:
+    retry_feature(feature_idx)
+  except:
+    retry_feature(feature_idx)
+  print("\tDone: "+ str(feature_idx + 1))
 
 print("<<START>>")
 pool = Pool()
