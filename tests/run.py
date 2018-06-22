11f116c7bd3fdd6c0f1677a14238f19bffbb5156
Ari Chivukula <ari@chivuku.la>
2018-06-21 17:24:39 -0700

    Update run.py

--- tests/run.py
+++ tests/run.py
@@ -3,7 +3,7 @@ from multiprocessing import Pool
 import os
 
 def retry_feature(feature_idx):
-  print("\tTrying: "+ str(feature_idx + 1))
+  print("Trying: "+ str(feature_idx + 1))
   os.system("lettuce --verbosity 1 -s " + str(feature_idx + 1) + " tests/features/TestExamples.feature")
 
 def test_feature(feature_idx):
@@ -11,10 +11,10 @@ def test_feature(feature_idx):
     try:
       retry_feature(feature_idx)
     except:
-      print sys.exc_info()[0]
-      print("\tFailed: "+ str(feature_idx + 1))
+      print(sys.exc_info()[0])
+      print("Failed: "+ str(feature_idx + 1))
     else:
-      print("\tSuccess: "+ str(feature_idx + 1))
+      print("Success: "+ str(feature_idx + 1))
       return
 
 print("<<START>>")
@@ -23,7 +23,7 @@ feature_idx = 0
 for line in open("tests/features/TestExamples.feature"):
   if line.startswith("  Scenario:"):
     feature_idx += 1
-    print("\tQueued: " + str(feature_idx))
+    print("Queued: " + str(feature_idx))
 pool.map(test_feature, range(feature_idx))
 pool.close()
 pool.join()
