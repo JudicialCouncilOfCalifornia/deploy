8c3825d9440ae0910ea1e0f1729cf5f60952ffb4
Ari Chivukula <ari@chivuku.la>
2018-06-21 17:49:33 -0700

    Update run.py

--- tests/run.py
+++ tests/run.py
@@ -1,10 +1,11 @@
 from __future__ import print_function
 from multiprocessing import Pool
 import os
+import sys
 
 def retry_feature(feature_idx):
-  print("Trying: "+ str(feature_idx + 1))
-  os.system("lettuce --verbosity 1 -s " + str(feature_idx + 1) + " tests/features/TestExamples.feature")
+  print("Trying: "+ str(feature_idx))
+  os.system("lettuce --verbosity 1 -s " + str(feature_idx) + " tests/features/TestExamples.feature")
 
 def test_feature(feature_idx):
   for x in range(3):
@@ -12,19 +13,14 @@ def test_feature(feature_idx):
       retry_feature(feature_idx)
     except:
       print(sys.exc_info()[0])
-      print("Failed: "+ str(feature_idx + 1))
+      print("Failed: "+ str(feature_idx))
     else:
-      print("Success: "+ str(feature_idx + 1))
+      print("Success: "+ str(feature_idx))
       return
 
 print("<<START>>")
 pool = Pool()
-feature_idx = 0
-for line in open("tests/features/TestExamples.feature"):
-  if line.startswith("  Scenario:"):
-    feature_idx += 1
-    print("Queued: " + str(feature_idx))
-pool.map(test_feature, range(feature_idx))
+pool.map(test_feature, range(int(sys.argv[-2]), int(sys.argv[-1])))
 pool.close()
 pool.join()
 print("<<END>>")
