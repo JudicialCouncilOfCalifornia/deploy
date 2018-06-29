85bf7971b2f6be82d8df118071095d8f13a01643
Ari Chivukula <ari@chivuku.la>
2018-06-28 20:48:16 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 eval $(aws ecr get-login --no-include-email --region us-west-2)
-docker run -e S3ENABLE=true -e S3BUCKET=$TF_VAR_NAME -e S3ACCESSKEY=$AWS_ACCESS_KEY_ID -e S3SECRETACCESSKEY=AWS_SECRET_ACCESS_KEY -e S3REGION=us-west-2 -d -p 80:80 617580300246.dkr.ecr.us-west-2.amazonaws.com/docassemble-server:master
+docker run -e S3ENABLE=true -e S3BUCKET=$TF_VAR_NAME -e S3ACCESSKEY=$AWS_ACCESS_KEY_ID -e S3SECRETACCESSKEY=AWS_SECRET_ACCESS_KEY -e S3REGION=$AWS_DEFAULT_REGION -d -p 80:80 617580300246.dkr.ecr.us-west-2.amazonaws.com/docassemble-server:master
