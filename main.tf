8ab08e04bd5355e01796afbb1fa3ff7f2ea06bb9
Ari Chivukula <ari@chivuku.la>
2018-07-12 14:30:35 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -107,7 +107,9 @@ resource "aws_vpc" "da_vpc" {
 }
 
 resource "aws_subnet" "da_subnet" {
+  count = "${length(data.aws_availability_zones.da_az.names)}"
   cidr_block = "${cidrsubnet(aws_vpc.da_vpc.cidr_block, 8, count.index)}"
+  availability_zone = "${data.aws_availability_zones.da_az.names[count.index]}"
   vpc_id = "${aws_vpc.da_vpc.id}"
   map_public_ip_on_launch = true
 }
