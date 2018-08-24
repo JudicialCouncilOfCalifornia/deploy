dd4f1bdcf78d5a2d3fc7331f35529697b0a4bf5d
Ari Chivukula <ari@chivuku.la>
2018-08-24 12:21:41 -0700

    Delete run-celery.sh

--- Docker/run-celery.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export DA_ROOT="${DA_ROOT:-/usr/share/docassemble}"
-source "${DA_PYTHON:-${DA_ROOT}/local}/bin/activate"
-
-export HOME=/var/www
-
-exec celery worker -A docassemble.webapp.worker --loglevel=DEBUG
