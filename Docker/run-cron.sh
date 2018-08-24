8e15222ce3cc7edd70d017c9001eb474bfd79361
Ari Chivukula <ari@chivuku.la>
2018-08-24 12:21:55 -0700

    Delete run-cron.sh

--- Docker/run-cron.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-export DA_ROOT="${DA_ROOT:-/usr/share/docassemble}"
-export DA_ACTIVATE="${DA_PYTHON:-${DA_ROOT}/local}/bin/activate"
-export HOME=/var/www
-export CRONTYPE=${1:-cron_daily}
-
-exec su -c "source $DA_ACTIVATE && python -m docassemble.webapp.cron -type $CRONTYPE" www-data
